30b2f6b3da7d2283c40e1d093b480bc42d46fe70
mcr <marionacaros@gmail.com>
2022-08-30 11:20:29 +0200

    add files

---
+++ data_proc/other/pdal_hag.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+for p in /home/m.caros/work/objectDetection/datasets/CAT3/w_towers_40x40_10p/tower_moved_*.las; do
+  pdal translate $p $p hag_nn --writers.las.extra_dims="HeightAboveGround=float32"
+  echo "$p"
+done
+for p in /home/m.caros/work/objectDetection/datasets/RIBERA/w_towers_40x40_10p/tower_moved_*.las; do
+  pdal translate $p $p hag_nn --writers.las.extra_dims="HeightAboveGround=float32"
+  echo "$p"
+done
+for p in /home/m.caros/work/objectDetection/datasets/BDN/w_towers_40x40_10p/tower_moved_*.las; do
+  pdal translate $p $p hag_nn --writers.las.extra_dims="HeightAboveGround=float32"
+  echo "$p"
+done
+
+for p in /home/m.caros/work/objectDetection/datasets/CAT3/w_othertowers_100x100_50p/tower_moved_*.las; do
+  pdal translate $p $p hag_nn --writers.las.extra_dims="HeightAboveGround=float32"
+  echo "$p"
+done
+for p in /home/m.caros/work/objectDetection/datasets/RIBERA/w_othertowers_100x100_50p/tower_moved_*.las; do
+  pdal translate $p $p hag_nn --writers.las.extra_dims="HeightAboveGround=float32"
+  echo "$p"
+done
+
+# Rename files
+#for f in /home/m.caros/work/objectDetection/datasets/CAT3/w_no_towers_40x40/*pkl.las; do
+#  mv -- "$f" "${f%.pkl.las}.las"
+#done
+# save HAG in z
+#for p in /dades/LIDAR/towers_detection/datasets/pc_towers_40x40/las_files/*HAG.las; do
+#  pdal translate $p $p hag_nn ferry --filters.ferry.dimensions="HeightAboveGround=Z"
+#  echo "$p"
+#done
