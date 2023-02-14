f4a336f30519101e4e94890d782b3b25b9d0d0ef
Mariona Carós <marionacaros@gmail.com>
2023-02-14 15:37:58 +0100

    Update 3_kmeans.py

--- data_proc/3_kmeans.py
+++ data_proc/3_kmeans.py
@@ -13,10 +13,10 @@ from progressbar import progressbar
 import random
 import multiprocessing
 
-i_path = '/dades/LIDAR/towers_detection/datasets/towers_100x100_cls'
-o_path = '/dades/LIDAR/towers_detection/datasets/kmeans_100x100_cls_c9_2048/'
+i_path = '/dades/LIDAR/towers_detection/datasets/pc_100x100_cls'
+o_path = '/dades/LIDAR/towers_detection/datasets/pc_100x100_cls_c9_2048/'
 N_POINTS = 2048
-NUM_CPUS = 10
+NUM_CPUS = 5
 
 # Tensorboard location and plot names
 now = datetime.datetime.now()
@@ -24,7 +24,7 @@ location = 'pointNet/runs/tower_detec/segmentation/'
 writer_tensorboard = SummaryWriter(location + now.strftime("%m-%d-%H:%M") + 'kmeans')
 
 
-def split_kmeans(file_path, n_points=N_POINTS, max_clusters=9, plot=False):
+def split_kmeans(file_path, n_points=N_POINTS, max_clusters=9, plot=False, fps_sample=False):
     """ split point cloud in windows of fixed size (n_points) with k-means
     Fill empty windows with duplicate points of previous windows
     Number of points must be multiple of n_points, so points left over are removed
@@ -43,8 +43,6 @@ def split_kmeans(file_path, n_points=N_POINTS, max_clusters=9, plot=False):
 
     with open(file_path, 'rb') as f:
         pc = pickle.load(f)
-    pc = pc[np.where(pc[:, 3] != 30)]
-    pc = pc[np.where(pc[:, 3] != 7)]
 
     # if point cloud is larger than n_points we cluster them with k-means
     if pc.shape[0] >= 2 * n_points:
@@ -53,7 +51,9 @@ def split_kmeans(file_path, n_points=N_POINTS, max_clusters=9, plot=False):
         pc_w = torch.FloatTensor()
 
         # K-means clustering
-        k_clusters = int(np.floor(in_pc.shape[0] / n_points))
+        k_clusters = int(np.ceil(in_pc.shape[0] / n_points))
+
+        # if there are more points, sample
         if k_clusters > max_clusters:
             k_clusters = max_clusters
             # Farthest point sampling
@@ -61,9 +61,19 @@ def split_kmeans(file_path, n_points=N_POINTS, max_clusters=9, plot=False):
             ix = random.sample(range(in_pc.shape[0]), n_points * max_clusters)
             in_pc = in_pc[ix, :]
 
+        # if not enough points, duplicate
+        elif in_pc.shape[0] < n_points * k_clusters:
+            points_needed = n_points * k_clusters - in_pc.shape[0]
+            rdm_list = np.random.randint(0, in_pc.shape[0], points_needed)
+            extra_points = in_pc[rdm_list, :]
+            in_pc = np.concatenate([in_pc, extra_points], axis=0)
+
         if in_pc.shape[0] % n_points != 0:
             # Number of points must be multiple of n_points, so points left over are removed
             in_pc = in_pc[:n_points * (in_pc.shape[0] // n_points), :]
+
+        if n_points*k_clusters > in_pc.shape[0]:
+            print(pc.shape)
 
         clf = KMeansConstrained(n_clusters=k_clusters, size_min=n_points, size_max=n_points,
                                 n_init=5, max_iter=10, tol=1e-2,
@@ -97,19 +107,20 @@ def split_kmeans(file_path, n_points=N_POINTS, max_clusters=9, plot=False):
     else:
         if pc.shape[0] > n_points:
             # Farthest point sampling
-            # pc = fps(pc, n_points)
+            if fps_sample:
+                pc = fps(pc, n_points)
             ix = random.sample(range(pc.shape[0]), n_points)
             pc = pc[ix, :, ]
         pc_w = torch.Tensor(pc).unsqueeze(2)
 
-    torch.save(pc_w, o_path + 'kmeans_' + filename + '.pt')
+    torch.save(pc_w, o_path + '_kmeans_' + filename + '.pt')
 
 
 def parallel_kmeans(files_list, num_cpus):
     p = multiprocessing.Pool(processes=num_cpus)
 
     for _ in progressbar(p.imap_unordered(split_kmeans, files_list, 1),
-                         max_value=len(files_list)): # redirect_stdout=True)
+                         max_value=len(files_list)):  # redirect_stdout=True)
         pass
     # p.imap_unordered(split_kmeans, files_list, 1)
     p.close()
